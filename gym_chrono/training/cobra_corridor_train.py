740915d58b6d5451d55b1ad279bc152ad82c25b4
jason <zzhou292@wisc.edu>
2023-10-05 09:27:03 -0500

    small

--- gym_chrono/training/cobra_corridor_train.py
+++ gym_chrono/training/cobra_corridor_train.py
@@ -20,10 +20,28 @@ if __name__ == '__main__':
     print(env.observation_space)
     print(env.action_space)
     print(env.action_space.sample())
+    
+    
+####### PARALLEL ##################
+
+    # num_cpu = 1
+    # # Create the vectorized environment
+    # env = SubprocVecEnv([make_env(i) for i in range(num_cpu)])
+    # model = PPO('MlpPolicy', env, verbose=1).learn(100000)
+###############################
+
+####### SEQUENTIAL ##################
+    model = PPO('MlpPolicy', env, verbose=1).learn(100000)
+###########################
+    model.save(f"PPO_cobra")
+    mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=100)
+    print(f"mean_reward:{mean_reward:.2f} +/- {std_reward:.2f}")
+
 
     # Hardcoded best agent: always go left!
     n_steps = 1000000
     for step in range(n_steps):
+        action, _ = model.predict(obs, deterministic=True)
         print(f"Step {step + 1}")
         obs, reward, terminated, truncated, info = env.step(
             env.action_space.sample())
